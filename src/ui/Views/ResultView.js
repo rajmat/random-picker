9287d75afcf168cd074983927e00a933abdef1fa
Shamila <sremtoula@simplon.co>
2019-08-29 12:15:33 +0200

    Add text and buttons animation

--- src/ui/Views/ResultView.js
+++ src/ui/Views/ResultView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { ResultText } from "../Text";
 
 const Container = styled.div`
@@ -18,6 +18,46 @@ const TextContainer = styled.div`
   align-items: center;
 `;
 
+const fadeInScale = keyframes`
+  0% {
+		transform: scale(0);
+		opacity: 0;		
+	}
+	60% {
+		transform: scale(1.1);	
+	}
+	80% {
+		transform: scale(0.9);
+		opacity: 1;	
+	}	
+	100% {
+		transform: scale(1);
+		opacity: 1;	
+	}
+`;
+
+const AnimatedResultText = styled(ResultText)`
+  animation: ${fadeInScale} 1s ease-in-out;
+  -webkit-animation: ${fadeInScale} 1s ease-in-out;
+  visibility: visible;
+`;
+
+const slideInBottom = keyframes`
+  0% {
+    opacity: 0;
+  }
+  80% {
+    -webkit-transform: translateY(80px);
+    transform: translateY(80px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+
 const ButtonsContainer = styled.div`
   height: 30vh;
   display: flex;
@@ -25,13 +65,20 @@ const ButtonsContainer = styled.div`
   justify-content: center;
   & > button {
     margin: 10px auto;
+    animation: ${slideInBottom} 2.2s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+  }
+  & > button:first-child {
+    animation-duration: 2s;
+  }
+  & > button:last-child {
+    animation-duration: 2.4s;
   }
 `;
 
 const ResultView = ({ result, children }) => (
   <Container>
     <TextContainer>
-      <ResultText>{result}</ResultText>
+      <AnimatedResultText>{result}</AnimatedResultText>
     </TextContainer>
     <ButtonsContainer>{children}</ButtonsContainer>
   </Container>
