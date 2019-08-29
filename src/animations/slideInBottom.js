8979a097dd233d5760aada916985421c26a7e520
Shamila <sremtoula@simplon.co>
2019-08-29 15:39:50 +0200

    update slideInBottom animation

--- src/animations/slideInBottom.js
+++ src/animations/slideInBottom.js
@@ -3,16 +3,19 @@ import { keyframes } from "styled-components";
 const slideInBottom = keyframes`
   0% {
     opacity: 0;
+    cursor: auto;
   }
   80% {
     -webkit-transform: translateY(80px);
     transform: translateY(80px);
     opacity: 0;
+    cursor: auto;
   }
   100% {
     -webkit-transform: translateY(0);
     transform: translateY(0);
     opacity: 1;
+    cursor: pointer;
   }
 `;
 
