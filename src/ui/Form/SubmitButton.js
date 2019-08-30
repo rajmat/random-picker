aeaede5466b4c7b0b78b9e5935f4bec5e3e6b5c9
Shamila <sremtoula@simplon.co>
2019-08-30 17:00:14 +0200

    remove unused import

--- src/ui/Form/SubmitButton.js
+++ src/ui/Form/SubmitButton.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { colors, fonts } from "../../variables";
+import { colors } from "../../variables";
 import { ContainedButton } from "../Buttons";
 
 const Button = styled(ContainedButton)`
